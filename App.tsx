cf4e5a92c9549ec18a38f5c6d8c5e6a109386e00
RennanD <rennan.douglas@objetivopi.com.br>
2021-04-19 11:38:51 -0300

    feat: Init project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(): JSX.Element {
+  return <Text>Hello NWL</Text>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
