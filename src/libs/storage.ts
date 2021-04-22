4fd2eb9cbde54d06ab210181b5a2b03245574fd7
RennanD <rennan.douglas@objetivopi.com.br>
2021-04-22 11:59:11 -0300

    refactor: format loadPlants returns

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { format } from 'date-fns';
+import { format } from 'date-fns';
 
 export interface PlantProps {
   id: number;
@@ -39,12 +39,27 @@ export async function savePlant(plant: PlantProps): Promise<void> {
   }
 }
 
-export async function loadPlants(): Promise<StoragePlantProps> {
+export async function loadPlants(): Promise<PlantProps[]> {
   try {
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
-    return plants;
+    const plantsSorted = Object.keys(plants)
+      .map(plant => ({
+        ...plants[plant].data,
+        hour: format(
+          new Date(plants[plant].data.datetimeNotification),
+          'HH:mm',
+        ),
+      }))
+      .sort((a, b) =>
+        Math.floor(
+          new Date(a.datetimeNotification).getTime() / 1000 -
+            Math.floor(new Date(b.datetimeNotification).getTime() / 1000),
+        ),
+      );
+
+    return plantsSorted;
   } catch (error) {
     throw new Error(error);
   }
