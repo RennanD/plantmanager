172a8ecfbcd80418a3de952210ea2d3e2cb8dce8
RennanD <rennan.douglas@objetivopi.com.br>
2021-04-23 13:43:25 -0300

    feat: add scheduled notifications on app

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,6 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { format } from 'date-fns';
 
+import * as Notifications from 'expo-notifications';
+
 export interface PlantProps {
   id: number;
   name: string;
@@ -19,12 +21,17 @@ export interface PlantProps {
 export interface StoragePlantProps {
   [id: string]: {
     data: PlantProps;
+    notificationId: string;
   };
 }
 
 export async function deletePlant(id: string): Promise<void> {
   const data = await AsyncStorage.getItem('@plantmanager:plants');
   const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+  await Notifications.cancelScheduledNotificationAsync(
+    plants[id].notificationId,
+  );
 
   delete plants[id];
 
@@ -33,12 +40,43 @@ export async function deletePlant(id: string): Promise<void> {
 
 export async function savePlant(plant: PlantProps): Promise<void> {
   try {
+    const nextTime = new Date(plant.datetimeNotification);
+    const now = new Date();
+
+    const { times, repeat_ever } = plant.frequency;
+
+    if (repeat_ever === 'week') {
+      const interval = Math.trunc(7 / times);
+      nextTime.setDate(now.getDate() + interval);
+    } else {
+      nextTime.setDate(nextTime.getDate() + 1);
+    }
+
+    const seconds = Math.abs(
+      Math.ceil((now.getTime() - nextTime.getTime()) / 1000),
+    );
+
+    const notificationId = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Heeey, 🌱',
+        body: `Está na hora de cuidadar da sua ${plant.name}`,
+        sound: true,
+        priority: Notifications.AndroidNotificationPriority.HIGH,
+        data: { plant },
+      },
+      trigger: {
+        seconds: seconds < 60 ? 60 : seconds,
+        repeats: true,
+      },
+    });
+
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
     const newPlant = {
       [plant.id]: {
         data: plant,
+        notificationId,
       },
     };
 
